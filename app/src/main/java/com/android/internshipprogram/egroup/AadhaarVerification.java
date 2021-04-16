28234ce938a970327ed2f9313bd5e91907733073
wideclassrooms <wideclassrooms@gmail.com>
2021-04-16 19:05:16 +0530

    group change added

--- app/src/main/java/com/android/internshipprogram/egroup/AadhaarVerification.java
+++ app/src/main/java/com/android/internshipprogram/egroup/AadhaarVerification.java
@@ -1,14 +1,16 @@
-package com.android.internshipprogram;
+package com.android.internshipprogram.egroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.android.internshipprogram.R;
 
 public class AadhaarVerification extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_aadhaar_verification);
+        setContentView(R.layout.e_group_activity_aadhaar_verification);
     }
 }
